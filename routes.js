ee026e36926cd88be896a32c868fe521183e4313
Nisarga <patel.nisarga1@gmail.com>
2015-04-11 14:23:56 -0400

    added twitter node api to app

--- routes.js
+++ routes.js
@@ -4,12 +4,44 @@ var request = require('request')
   , util = require('util')
   , TwitterStrategy = require('passport-twitter').Strategy;
 
+
+var twitterAPI = require('node-twitter-api');
+var twitter = new twitterAPI({
+    consumerKey: "RRVoDZUDwyWn7vVrNqo02c7HQ",
+    consumerSecret: "Wxy758B8OqVkfQbMcbWwyXx5oXcrT0Ibxgg8UT9b4Ok0bkevjP",
+    callback: "http://twitterfr.azurewebsites.net/auth/twitter/callback"
+});
+
+var g_Rtoken;
+var g_RtokenSecret;
+twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+    if (error) {
+        console.log("Error getting OAuth request token : " + error);
+    } else {
+        //store token and tokenSecret somewhere, you'll need them later; redirect user 
+        g_Rtoken = requestToken;
+        g_RtokenSecret = requestTokenSecret;
+    }
+});
+
+var g_Atoken;
+var g_AtokenSecret;
+twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        //store accessToken and accessTokenSecret somewhere (associated to the user) 
+        g_Atoken = requestToken;
+        g_AtokenSecret = requestTokenSecret;
+        //Step 4: Verify Credentials belongs here 
+    }
+});
+
 var router = express.Router();
 var cookieParser = require('cookie-parser');
 
 router.use(cookieParser());
-var g_token;
-var g_tokenSecret;
+
 var tws = new TwitterStrategy({
     consumerKey: "RRVoDZUDwyWn7vVrNqo02c7HQ",
     consumerSecret: "Wxy758B8OqVkfQbMcbWwyXx5oXcrT0Ibxgg8UT9b4Ok0bkevjP",
